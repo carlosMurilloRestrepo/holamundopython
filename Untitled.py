e979454f6a9d990b005bd6252fe208d1a3e24bf4
Carlos Murillo <krlosmurillo199@gmail.com>
2016-08-08 21:12:03 -0500

    Creación Proyecto

---
+++ Untitled.py
@@ -0,0 +1,96 @@
+
+# coding: utf-8
+
+# In[1]:
+
+print "Hola Mundo"
+
+
+# $$\sum_{p}^{}li(x^p)-log(2)$$
+
+# In[7]:
+
+2 + 4 * 6 / 8.0 -3
+
+
+# In[9]:
+
+3.0 / 2.0
+
+
+# # hola 
+
+# In[10]:
+
+3+5j
+
+
+# In[11]:
+
+3+5j-1-1j
+
+
+# In[13]:
+
+abs (3+5j)
+
+
+# In[14]:
+
+5**3
+
+
+# In[15]:
+
+int(_13)
+
+
+# In[16]:
+
+float(_15)
+
+
+# In[17]:
+
+complex(_15)
+
+
+# In[19]:
+
+round(_13)
+
+
+# In[20]:
+
+max(8,10,20,30)
+
+
+# In[21]:
+
+x,y,z,a=1,2,3,4
+
+
+# In[22]:
+
+print y
+
+
+# In[23]:
+
+x,y,z,a=a,z,y,x
+
+
+# In[37]:
+
+x=9
+y=8
+if x>y:
+    print"El Mayor Es",x
+else:
+    print"El Mayor Es",y
+
+
+# In[ ]:
+
+
+
